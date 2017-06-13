a16413ac12f6266b4ac61d224d17aab3f2110f20
TakashiIshii <tishii@aiming-inc.com>
2017-06-13 16:39:54 +0900

    add build script

---
+++ build_grpc_c_core_library.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "Usage: build_grpc_c_core_library.sh BUILD_TYPE(debug or release)" 1>&2
+    exit 1
+fi
+
+if [ $1 != 'debug' ] && [ $1 != 'release' ]; then
+    echo "Usage: build_grpc_c_core_library.sh BUILD_TYPE(debug or release)" 1>&2
+    exit 1
+fi
+
+BUILD_TYPE=$1
+
+current_dir=$(cd $(dirname $0) && pwd)
+work_dir="build"
+
+#build library for iOS
+cd "$current_dir/xcprojects/grpc_csharp_ext"
+
+xcodebuild -workspace grpc_csharp_ext.xcworkspace \
+           -scheme grpc_csharp_ext \
+           -configuration $BUILD_TYPE \
+           -derivedDataPath "$work_dir" \
+           clean build
